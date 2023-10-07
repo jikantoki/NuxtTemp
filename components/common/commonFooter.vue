c8160173f9bbe3d6cb3204fbb5e236b8e8728b9b
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-10-07 23:55:08 +0900

    仮置き

---
+++ components/common/commonFooter.vue
@@ -0,0 +1,13 @@
+<template lang="pug">
+.center
+  .copyRight &copy; 2019 - {{ new Date().getFullYear() }} エノキ電気
+</template>
+
+<script>
+import mixins from '@/functions/mixins'
+export default {
+  mixins: [mixins]
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ pages/HomeView.vue
@@ -0,0 +1,18 @@
+<template lang="pug">
+.home
+  //img(alt="Vue logo" src="@/assets/logo.png")
+  HelloWorld(msg="Welcome to Your Vue.js App")
+</template>
+
+<script>
+// @ is an alias to /src
+import mixins from '/functions/mixins'
+
+export default {
+  name: 'HomeView',
+  components: {
+    HelloWorld
+  },
+  mixins: [mixins]
+}
+</script>
