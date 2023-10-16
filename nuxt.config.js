21d078c96255dcb8de929b77a1c31c54dd9143d8
jikantoki <61790160+jikantoki@users.noreply.github.com>
2023-10-16 14:10:44 +0900

    名称変更

--- nuxt.config.js
+++ nuxt.config.js
@@ -7,8 +7,8 @@ export default {
   },
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    titleTemplate: '%s - NuxtTemp',
-    title: 'NuxtTemp',
+    titleTemplate: '%s - NuxTemp',
+    title: 'NuxTemp',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
